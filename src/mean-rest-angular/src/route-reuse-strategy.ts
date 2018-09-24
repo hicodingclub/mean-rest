25c073a768748337214a83d175f9f57d77308745
Xibin Liu <xibinliu@cienet.com.cn>
2018-09-24 09:53:16 -0700

    mean-rest-angular: when component is re-used, scroll back to the original position

--- src/mean-rest-angular/src/route-reuse-strategy.ts
+++ src/mean-rest-angular/src/route-reuse-strategy.ts
@@ -3,11 +3,17 @@ import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from 
 const COMPONENT_CACHE_DURATION = 30 * 1000;
 export class MraRouteReuseStrategy implements RouteReuseStrategy {
 
-    detachedRouteHandles: { [key: string]: any[] } = {};
+    detachedRouteHandles: { [key: string]: any[] } = {}; //key is url, and value is at [handle, timestamp] format
+    pageYOffset = {};
     editItems = {};
 
     /** Determines if this route (and its subtree) should be detached to be reused later */
     public shouldDetach(route: ActivatedRouteSnapshot): boolean {
+        if (route.routeConfig && route.routeConfig.path === 'list') {
+            //save current scroll position
+            let key = route['_routerState'].url;
+            this.pageYOffset[key] = window.pageYOffset;
+        }
         return route.routeConfig.path === 'list';
     }
 
@@ -44,11 +50,17 @@ export class MraRouteReuseStrategy implements RouteReuseStrategy {
         }
         if (!this.detachedRouteHandles[key]) return null;
         if (date.getTime() - this.detachedRouteHandles[key][1]  > COMPONENT_CACHE_DURATION) return null;
+        
+        let yOffset = this.pageYOffset[key];
+        setTimeout(function () {
+                console.log("==retrieve: ", key, yOffset);
+                window.scrollTo(0, yOffset);
+            }, 20); //scroll to saved position
+        
         return this.detachedRouteHandles[key][0];
     }
 
     /** Determines if a route should be reused */
-    //future: to be created page; curr: existing page.
     public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
         // Below is the default implementation;
         return future.routeConfig === curr.routeConfig
