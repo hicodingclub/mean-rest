1151f76d338d8e36b59b81f2c9a8babb1b92d2f6
Xibin Liu <xibinliu@yahoo.com>
2019-05-31 16:09:46 -0700

    Support list/grid view action icon highlight

--- src/node.js/express/auth-app/lib/authz.js
+++ src/node.js/express/auth-app/lib/authz.js
@@ -253,8 +253,9 @@ const verifyRolePermission = function(req, res, next) {
 const verifyPermission = function(req, res, next) {
   const loggedIn = !!req.muser
   console.log("***req.muser", req.muser)
-  console.log("***req.mddsModuleAccesses", req.mddsModuleAccesses)
-    
+  console.log("***req.mddsModuleAccesses - public access\n", req.mddsModuleAccesses)
+  console.log("***req.mddsModulePermissions - role access\n", req.mddsModulePermissions)
+
   if (loggedIn && req.muser.role) {
     return verifyRolePermission(req, res, next); //use role based permission
   } 
