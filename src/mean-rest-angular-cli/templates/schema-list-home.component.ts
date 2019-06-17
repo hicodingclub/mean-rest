88d8ac2c3e7ca1e3355ffb0ac3bf41a19bf62cf0
Xibin Liu <xibinliu@yahoo.com>
2019-06-17 13:00:02 -0700

    mra: support sorting for list view

---
+++ src/mean-rest-angular-cli/templates/schema-list-home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, ActivatedRoute }    from '@angular/router';
+import { Injector } from '@angular/core';
+
+import { <%-SchemaName%>ListComponent } from './<%-schemaName%>-list.component';
+import { <%-SchemaName%>Service } from '../<%-schemaName%>.service';
+
+@Component({
+  selector: 'app-<%-schemaName%>-list-sub',
+  templateUrl: './<%-schemaName%>-list-sub.component.html',
+  styleUrls: ['./<%-schemaName%>-list.component.css']
+})
+export class <%-SchemaName%>ListSubComponent extends <%-SchemaName%>ListComponent implements OnInit {
+  private parentData = {};
+  constructor(
+      protected <%-schemaName%>Service: <%-SchemaName%>Service,
+      protected injector: Injector,
+      protected router: Router,
+      protected route: ActivatedRoute,
+      protected location: Location) {
+        super(<%if (schemaHasRef) {%>null,<%}%> <%-schemaName%>Service, injector, router, route, location);
+  }
+
+  ngOnInit() {
+      let ref = this.getParentRouteRefField();
+      let id = this.getParentRouteItemId();
+      this.detail = {};
+
+    if (this.arrayFields.some(x=>x[0] == ref)) {
+          this.parentData[ref] = {'selection':[{'_id': id}] }; 
+          this.detail[ref] = {'selection':[{'_id': id}] }; //search on array list
+      } else {
+          this.parentData[ref] = {'_id': id };
+          this.detail[ref] = {'_id': id }; //make this as the search context
+      }
+      this.processSearchContext();
+      this.populateList();
+  }
+}
