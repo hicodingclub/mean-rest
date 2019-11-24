d0c9f34e88d9f516a5504ce76e1195aa5baffe93
Xibin Liu <xibinliu@yahoo.com>
2019-11-23 18:02:49 -0800

    Emailing: setting model, and logic to send email to ses

--- src/node.js/express/emailing/index.js
+++ src/node.js/express/emailing/index.js
@@ -3,30 +3,46 @@ const meanRestExpress = require('@hicoder/express-core');
 
 const emailSystemDef = require('./model');
 
+function modelExecuteSuccess(taskStr) {
+  function doSomething(result) {
+    console.log(" --- Emailing: model excecute succeeded: ", taskStr);
+  }
+  return doSomething;
+}
+function modelExecuteError(taskStr) {
+  function doSomething(err) {
+    if (err.code === 11000) console.log(" --- Emailing: model excecute already exist: ", taskStr);
+    else if (err.errmsg) console.warn(" --- Emailing: model excecute failed: ", taskStr, err.errmsg);
+    else console.warn(" --- Emailing: model excecute failed: ", taskStr, err.message);
+  }
+  return doSomething;
+}
+function populateSettingsToDB() {
+  const router =  meanRestExpress.RestRouter(emailSystemDef, 'internal-template-usage');
+  const restController = router.restController;
+  
+  async function runDB() {  
+    const takInfo = `create email setting ...`;
+    await restController.ModelExecute(
+            "emailSettings",
+            "create",
+            {settingName: 'Settings',
+             defaultSender: 'sampleemailxibinliu@gmail.com'} //document
+        ).then(modelExecuteSuccess(takInfo), modelExecuteError(takInfo));
+  }
+  
+  runDB();
+}
+
 class MddsEmailer {
   constructor(configJsonFile) {
     this.configJsonFile = configJsonFile;
-    AWS.config.loadFromPath(configJsonFile);;
+    AWS.config.loadFromPath(configJsonFile);
   }
 
   populateTemplatesToDB(templates) {
     const router =  meanRestExpress.RestRouter(emailSystemDef, 'internal-template-usage');
     const restController = router.restController;
-
-    function modelExecuteSuccess(taskStr) {
-      function doSomething(result) {
-        console.log(" --- Emailing: model excecute succeeded: ", taskStr);
-      }
-      return doSomething;
-    }
-    function modelExecuteError(taskStr) {
-      function doSomething(err) {
-        if (err.code === 11000) console.log(" --- Emailing: model excecute already exist: ", taskStr);
-        else if (err.errmsg) console.warn(" --- Emailing: model excecute failed: ", taskStr, err.errmsg);
-        else console.warn(" --- Emailing: model excecute failed: ", taskStr, err.message);
-      }
-      return doSomething;
-    }
     
     async function runDB() {  
       for (let temp of templates) {
@@ -43,29 +59,26 @@ class MddsEmailer {
   }
 
   async sendEmailTemplate(to, templateTag, infoObj) {
-    const router =  meanRestExpress.RestRouter(emailTemplateDef, 'internal-template-usage');
+    const router =  meanRestExpress.RestRouter(emailSystemDef, 'internal-template-usage');
     const restController = router.restController;
 
     let template;
-    let msg = '';
-    await restController.ModelExecute(
-      "emailTemplate",
-      "findOne",
-      {tag: templateTag}//search criteria
-    ).then(
-      function(result) {
-        if (result) template = result;
-      }, 
-      function (err) {
-        msg = err.message;;
-      }
-    );
 
-    if (!template) {
-      return new Promise(function(resolve, reject) {
-        const err = new Error(`Template not found. ${msg}`);
-        reject(err);
-      });
+    let success = 0, fail = 0, errors = [];
+
+    try {
+      template = await restController.ModelExecute(
+        "emailTemplate",
+        "findOne",
+        {tag: templateTag}, //search criteria
+      );
+
+      if (!template) {
+        throw new Error(`Template not found ${templateTag}`);
+      }
+    } catch(err) {
+      errors.push(err);
+      return {success, fail, errors};
     }
 
     let content = template.content;
@@ -76,85 +89,118 @@ class MddsEmailer {
 
     content = content.replace(/http(s)?:\/\/(http(s)?:\/\/)/g, '$2');
 
-    // Create sendEmail params 
-    const params = {
-      Destination: { /* required */
-        CcAddresses: [
-          /* more items */
-        ],
-        ToAddresses: to, // array
-      },
-      Message: { /* required */
-        Body: { /* required */
-          Html: {
-          Charset: "UTF-8",
-          Data: content,
-          },
-          Text: {
-          Charset: "UTF-8",
-          Data: content,
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: template.subject,
-        }
-        },
-      Source: template.fromEmail, /* required */
-      /*
-      ReplyToAddresses: [
-        'EMAIL_ADDRESS',
-        // more items 
-      ],
-      */
-    };
-    // Create the promise and SES service object
-    const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-    return sendPromise;
+    const result = await this.sendEmail(template.fromEmail, to, template.subject, content);
+    return result;
   }
 
-  sendEmail(from, to, subject, content) {
-    // Create sendEmail params 
-    const params = {
-      Destination: { /* required */
-        CcAddresses: [
-          /* more items */
-        ],
-        ToAddresses: to, // array
-      },
-      Message: { /* required */
-        Body: { /* required */
-          Html: {
-          Charset: "UTF-8",
-          Data: content,
-          },
-          Text: {
-          Charset: "UTF-8",
-          Data: content,
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: subject,
+  async sendEmail(from, to, subject, content) {
+    let success = 0, fail = 0, errors = [];
+
+    if (!from) {
+      const router =  meanRestExpress.RestRouter(emailSystemDef, 'internal-template-usage');
+      const restController = router.restController;
+  
+      try {
+        const setting = await restController.ModelExecute(
+          "emailSettings",
+          "findOne",
+          {}//search criteria
+        );
+
+        if (!setting) {
+          throw new Error('no email settings found');
         }
+
+        from = setting.defaultSender;
+      } catch(err) {
+        errors.push(err);
+        return {success, fail, errors};
+      }
+    }
+
+    // deduplicate
+    const uniqSet = new Set(to);
+    const uniqArr = [...uniqSet];
+
+    function sendEmailPromise(num, parameters) {
+      const promise = new Promise( (resolve, reject) => {
+        new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(parameters).promise().then(
+          (re) => {
+            resolve({result: 'success', num});
+          }
+        ).catch(
+          (er) => {
+            resolve({result: 'fail', num, error: er});
+          }
+        );
+      });
+      return promise;
+    }
+
+    const recipientsPerGroup = 45; //AWS limit is 50
+    const groupNum = Math.floor(uniqArr.length / recipientsPerGroup) + 1;
+    
+    const promises = [];
+    for (let i = 0; i < groupNum; i++) {
+      const start = i * recipientsPerGroup;
+      const end = (i + 1) * recipientsPerGroup;
+      const grp = uniqArr.slice(start, end);
+      // Create sendEmail params 
+      const params = {
+        Destination: { /* required */
+          CcAddresses: [
+            /* more items */
+          ],
+          ToAddresses: uniqArr.length > 1 ? [] : grp, // array
+          BccAddresses: uniqArr.length > 1 ? grp : [], // array
         },
-      Source: from, /* required */
-      /*
-      ReplyToAddresses: [
-        'EMAIL_ADDRESS',
-        // more items 
-      ],
-      */
-    };
-    // Create the promise and SES service object
-    const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-    return sendPromise;
+        Message: { /* required */
+          Body: { /* required */
+            Html: {
+            Charset: "UTF-8",
+            Data: content,
+            },
+            Text: {
+            Charset: "UTF-8",
+            Data: content,
+            }
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: subject,
+          }
+          },
+        Source: from, /* required */
+        /*
+        ReplyToAddresses: [
+          'EMAIL_ADDRESS',
+          // more items 
+        ],
+        */
+      };
+      promises.push(sendEmailPromise(grp.length, params));
+    }
+
+    // will not throw errors - see above sendEmailPromise funciton
+    const results = await Promise.all(promises);
+
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      if (result.result === "success") {
+        success += result.num;
+      } else if (result.result === "fail") {
+        fail += result.num;
+        errors.push(result.error);
+      }
+    }
+    return {success, fail, errors};
   }
 }
 
 //used to manage the public access
 function GetEmailingManageRouter(moduleName, authAppFuncs) {
   const router =  meanRestExpress.RestRouter(emailSystemDef, moduleName, authAppFuncs);
+  populateSettingsToDB();
   return router;
 }
 
--- src/node.js/express/emailing/model.js
+++ src/node.js/express/emailing/model.js
@@ -15,19 +15,20 @@ const emailTemplateSchema = new Schema({
     lowercase: true,
     //required: 'Email address is required',
     validate: [validateEmail, 'Please fill a valid email address'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'], 
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    description: 'Leave it blank if default sender email address in email settings is used'
   },
   subject: { type: String, required: true },
   content: { type: String, editor: true, required: true },
   tag: { type: String, required: true, unique: true },
 }, {timestamps: true});
 
-const brief = "templateName fromEmail subject tag";
-const detail = "templateName fromEmail subject content tag";
-const create = "templateName fromEmail subject content tag";
-const edit = "templateName fromEmail subject content tag";
-const textSearch = "templateName fromEmail subject content tag";
-const index = "templateName";
+const brief = 'templateName fromEmail subject tag';
+const detail = 'templateName fromEmail subject content tag';
+const create = 'templateName fromEmail subject content tag';
+const edit = 'templateName fromEmail subject content tag';
+const textSearch = 'templateName fromEmail subject content tag';
+const index = 'templateName';
 
 const emailLogSchema = new Schema({
   from: { type: String },
@@ -40,32 +41,62 @@ const emailLogSchema = new Schema({
   module: { type: String, required: true },
   reason: { type: String, required: true },
   result: { type: String, required: true },
-  userId: { type: String }
+  userId: { type: String },
 }, {timestamps: true});
 
-const brief2 = "to module reason result userId createdAt";
-const detail2 = "from to subject content template module reason result userId createdAt";
-const create2 = "subject content template";
-const edit2 = "subject content template";
-const textSearch2 = "templateName fromEmail subject content tag";
-const index2 = "module";
+const brief2 = 'to module reason result userId createdAt';
+const detail2 = 'from to subject content template module reason result userId createdAt';
+const create2 = 'subject content template';
+const edit2 = 'subject content template';
+const textSearch2 = 'subject content';
+const index2 = 'module';
+
+const emailSettingsSchema = new Schema({
+  settingName: { type: String, required: true },
+  hiddenUnique: { type: String, default: 'unique'}, // hidden filed to make sure it is unique
+  defaultSender: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true, // 'Email address is required',
+    validate: [validateEmail, 'Please fill a valid email address'],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    description: 'Default sender email address. Must be validated by email service for it to take effect.'
+  },
+});
+//to make the association unique
+emailSettingsSchema.index({ hiddenUnique: 1}, {unique: true}); // schema level
+
+const brief3 = 'settingName defaultSender[Default Sender Email Address]';
+const detail3 = 'settingName defaultSender[Default Sender Email Address]';
+const create3 = 'settingName defaultSender[Default Sender Email Address]';
+const edit3 = 'defaultSender[Default Sender Email Address]';
+const textSearch3 = 'defaultSender';
+const index3 = 'settingName';
 
 const schemas = {
-  "emailTemplate": {
+  'emailTemplate': {
     schema: emailTemplateSchema,
     views: [brief, detail, create, edit, textSearch, index],
     name: 'Email Template'
   },
-  "emailLog": {
+  'emailSettings': {
+    schema: emailSettingsSchema,
+    views: [brief3, detail3, create3, edit3, textSearch3, index3],
+    name: 'Email Settings',
+    singleRecord: true, //single record for configuration
+    api: 'LRU',
+  },
+  'emailLog': {
     schema: emailLogSchema,
     views: [brief2, detail2, create2, edit2, textSearch2, index2],
     name: 'Email Log',
-    api: "LR",
+    api: 'LR',
   },
 };
 
-const dateFormat = "MM-DD-YYYY";
-const timeFormat = "hh:mm:ss";
+const dateFormat = 'MM-DD-YYYY';
+const timeFormat = 'hh:mm:ss';
 
 const config = {
   dateFormat: dateFormat,
@@ -73,9 +104,9 @@ const config = {
 }
 
 const authz = {
-  "module-authz": {"LoginUser": {"others": "R", "own": "R"}, "Anyone": ""},
-  "emailTemplate": {"LoginUser": {"others": "R", "own": "R"}, "Anyone": ""},
-  "emailLog": {"LoginUser": {"others": "", "own": "R"}, "Anyone": ""},
+  'module-authz': {'LoginUser': {'others': 'R', 'own': 'R'}, 'Anyone': ''},
+  'emailTemplate': {'LoginUser': {'others': 'R', 'own': 'R'}, 'Anyone': ''},
+  'emailLog': {'LoginUser': {'others': '', 'own': 'R'}, 'Anyone': ''},
 }
 
 module.exports = {schemas: schemas, config: config, authz: authz}
