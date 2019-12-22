012e137620e069f125b906d0f7710ace7de1d6bc
Xibin Liu <xibinliu@yahoo.com>
2019-12-21 20:34:55 -0800

    angular-core: fix max number validator bug

--- src/ui/angular/projects/core/src/lib/mdds-common.directives.ts
+++ src/ui/angular/projects/core/src/lib/mdds-common.directives.ts
@@ -14,7 +14,7 @@ export class MddsMinNumberDirective implements Validator {
     if (isNaN(minnum)) { return null; } // only compare when max is a number
     // only compare when inputs are numbers
     if (typeof num === 'number' && num < minnum) {
-        return { libMddsMinNumber: 'must be no less than ' + minnum };
+        return { libMddsMinNumber: `must not be less than ${minnum}` };
     }
     return null;
   }
@@ -25,15 +25,15 @@ export class MddsMinNumberDirective implements Validator {
   providers: [{provide: NG_VALIDATORS, useExisting: MddsMaxNumberDirective, multi: true}]
 })
 export class MddsMaxNumberDirective implements Validator {
-  @Input() libMddsMaxnumber: string;
+  @Input() libMddsMaxNumber: string;
 
   validate(control: AbstractControl): ValidationErrors | null {
     const num = control.value;
-    const maxnum = parseFloat(this.libMddsMaxnumber);
+    const maxnum = parseFloat(this.libMddsMaxNumber);
     if (isNaN(maxnum)) { return null; } // only compare when max is a number
     // only compare when inputs are numbers
     if (typeof num === 'number' && num > maxnum) {
-        return { libMddsMaxnumber: 'must be no greater than ' + maxnum };
+        return { libMddsMaxNumber: `must not be greater than ${maxnum}` };
     }
     return null;
   }
